e24c07d9ab2ff44b5f6cf52db4c399b3d13230d6
Mancorl <mjaudun@hotmail.com>
2025-08-01 14:14:07 +0200

    Prosjekt 3 starter nå i fullskjerm

--- Prosjekt_3_2d_sidescroller/Andromeda_Expedition.py
+++ Prosjekt_3_2d_sidescroller/Andromeda_Expedition.py
@@ -1,2 +1,29 @@
 import pygame
+def main():
+    __init__()#initialiserer pygame
+    screen = get_screen()#Lager lerretet vi kan tegne spillet på
+    run(screen)#kjører selve spillet
 
+def __init__():
+    pygame.init()
+    pygame.display.set_caption("Andromeda Expedition")
+
+def get_screen():
+    stats = pygame.display.Info()
+    screen_w = stats.current_w
+    screen_h = stats.current_h
+    screen = pygame.display.set_mode((screen_w,screen_h), pygame.FULLSCREEN)
+    return screen
+
+
+def run(screen):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                running = False
+    
+
+
+
+main()
