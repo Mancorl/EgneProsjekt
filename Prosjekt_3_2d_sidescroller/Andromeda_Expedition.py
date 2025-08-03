740da2a15e65c171a7b485218b0bbd06870dfe47
Audun Steinkopf <Mjaudun@localhost.localdomain>
2025-08-03 16:43:49 +0200

    La til instillingsskjerm, med mulighet til å gå tilbake til hovedmenyen, refaktorerte splashscreen, tekst og valgbokser

--- Prosjekt_3_2d_sidescroller/Andromeda_Expedition.py
+++ Prosjekt_3_2d_sidescroller/Andromeda_Expedition.py
@@ -5,6 +5,7 @@ def main():
     screen = get_screen()#Lager lerretet vi kan tegne spillet på
     run(screen)#kjører selve spillet
 
+
 def __init__():
     pygame.init()
     pygame.display.set_caption("Andromeda Expedition")
@@ -16,42 +17,72 @@ def get_screen():
     screen = pygame.display.set_mode((screen_w,screen_h), pygame.FULLSCREEN)
     return screen
 
-def splash_page(screen):
+def firkant(koord_vr, koord_lr, bredde, høyde, screen, farge):
+    firkanten = pygame.Rect(koord_vr, koord_lr, bredde, høyde)
+    pygame.draw.rect(screen,farge, firkanten)
+    return firkanten
+
+def splash_text(screen, text_string, Chosen_font = "Arial",font_size = 80, color=(255,255,255), factoroffset=1, height_adjust = 0):
     w,h = pygame.display.get_surface().get_size()
-    font = pygame.font.SysFont(pygame.font.get_default_font(),80)
-    splash_string = "Laget av Audun Steinkopf i pygame"
-    text = font.render(splash_string,True, (255,255,255))
-    (wt,ht) = font.size(splash_string)
-    screen.blit(text,(w/2 - wt/2 ,h/2 - ht/2))
+    font = pygame.font.SysFont(Chosen_font, font_size)
+    text = font.render(text_string,True, color)
+    (wt,ht) = font.size(text_string)
+    screen.blit(text,(w/2 - wt/2 ,h/2 - ht/2 * factoroffset + height_adjust))
+    return (wt,ht)
+
+def splash_page(screen):
+    splash_text(screen,"Laget av Audun Steinkopf i pygame")
     pygame.display.update()
-    time.sleep(3)
+    time.sleep(1)
 
 def main_menu(screen):
     #Definerer verdiene som brukes for å vise alternativene på hovedmenyen
+    farge1 = (255,255,255)
+    farge2 = (0,0,0)
     w,h = pygame.display.get_surface().get_size()
     Exit_height_adjust = h/3
-    font = pygame.font.SysFont(pygame.font.get_default_font(),80)
-    Exit = "Avslutt Spillet"
-    text = font.render(Exit,True, (255,255,255))
-    (wt,ht) = font.size(Exit)
+    (wt,ht) = splash_text(screen, "Avslutt Spillet", height_adjust=Exit_height_adjust)
+    (wi, hi) = splash_text(screen, "Instillinger", factoroffset=9, height_adjust=Exit_height_adjust)
 
 
     #Definerer og tegner selve alternativene
-    avsluttfirkant2 = pygame.Rect(w/2 - wt *1.1, h/2 - ht*1.1 + Exit_height_adjust, wt * 2.2, ht * 2.2)
-    pygame.draw.rect(screen,(255,255,255), avsluttfirkant2)
-    avsluttfirkant1 = pygame.Rect(w/2 - wt, h/2 - ht + Exit_height_adjust, wt * 2, ht * 2)
-    pygame.draw.rect(screen,(0,0,0), avsluttfirkant1)
-    screen.blit(text,(w/2 - wt/2 ,h/2 - ht/2 + Exit_height_adjust))
+    avsluttfirkant2 = firkant(w/2 - wt *1.1, h/2 - ht*1.1 + Exit_height_adjust, wt * 2.2, ht * 2.2, screen, farge1)
+    avsluttfirkant1 = firkant(w/2 - wt, h/2 - ht + Exit_height_adjust, wt * 2, ht * 2, screen, farge2)
+
+    Instillingsfirkant2 = firkant(w/2 - wt *1.1, h/2 - ht*5.1 + Exit_height_adjust, wt * 2.2, ht * 2.2, screen, farge1)
+    Instillingsfirkant1 = firkant(w/2 - wt, h/2 - ht*5 + Exit_height_adjust, wt * 2, ht * 2, screen, farge2)
+
+    splash_text(screen, "Avslutt Spillet", height_adjust=Exit_height_adjust)
+    splash_text(screen, "Instillinger", factoroffset=9, height_adjust=Exit_height_adjust)
+
+
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            (x,y) = pygame.mouse.get_pos()
-            print(x,y)
-            if avsluttfirkant1.collidepoint(x,y):
+            if avsluttfirkant1.collidepoint(pygame.mouse.get_pos()):
                 return "quit"
+            if Instillingsfirkant1.collidepoint(pygame.mouse.get_pos()):
+                return "instillinger"
 
         #Dersom ingenting annet blir valgt fortsetter løkken som før
     return "main_menu"
 
+def instillinger(screen):
+    farge1 = (255,255,255)
+    farge2 = (0,0,0)
+    w,h = pygame.display.get_surface().get_size()
+    Exit_height_adjust = h/3
+
+    (wt,ht) = splash_text(screen, "Tilbake", height_adjust=Exit_height_adjust)
+    avsluttfirkant2 = firkant(w/2 - wt *1.1, h/2 - ht*1.1 + Exit_height_adjust, wt * 2.2, ht * 2.2, screen, farge1)
+    avsluttfirkant1 = firkant(w/2 - wt, h/2 - ht + Exit_height_adjust, wt * 2, ht * 2, screen, farge2)
+    splash_text(screen, "Avslutt Spillet", height_adjust=Exit_height_adjust)
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if avsluttfirkant1.collidepoint(pygame.mouse.get_pos()):
+                return "main_menu"
+
+    return "instillinger"
 
 
 def run(screen):
@@ -62,11 +93,11 @@ def run(screen):
     while running:
         screen.fill((0,0,0))
         if state == "main_menu":
-            choice = main_menu(screen)
-            if choice != "main_menu":
-                state = choice
+            state = main_menu(screen)
         if state == "quit":
             running = False
+        if state == "instillinger":
+            state = instillinger(screen)
         pygame.display.update()
     
 
